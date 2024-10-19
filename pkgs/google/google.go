5dbb7ceaeb4539635bde7e470e2d60bbcdec596c
thanhnc2 <thanhnc2@sendo.vn>
2024-10-20 01:33:46 +0700

    chore: update msg

--- pkgs/google/google.go
+++ pkgs/google/google.go
@@ -49,7 +49,7 @@ func (c *Client) SendMsg(j Msg, thread bool) error {
 	jira_uri := os.Getenv("JIRA_HOST") + "/browse/" + j.IssueId
 	summary := j.Summary
 	msgId := "client-" + strings.ToLower(j.IssueId)
-	message := "Hi <users/" + j.UserFedId + "> This task need your action \nStatus:" + j.Status + "\n*" + summary + "* \nLink:" + jira_uri
+	message := "*"+ summary + "* \nHi <users/" + j.UserFedId + "> This task need your action \nStatus:" + j.Status + "\nLink:" + jira_uri
 	if thread {
 		if j.ParentId == "" {
 			msgId = "client-" + strings.ToLower(j.IssueId) + j.ChangelogId
@@ -80,7 +80,7 @@ func (c *Client) UpdateMsg(j Msg) {
 
 	name := "spaces/" + space + "/messages/" + msgId
 	jira_uri := os.Getenv("JIRA_HOST") + "/browse/" + j.IssueId
-	message := "Hi <users/" + j.UserFedId + "> This task need your action \nStatus:" + j.Status + "\n" + summary + "\nLink:" + jira_uri
+	message := "*"+ summary + "* \nHi <users/" + j.UserFedId + "> This task need your action \nStatus:" + j.Status + "\nLink:" + jira_uri
 	msg := chat.Message{
 		Text: message,
 		Thread: &chat.Thread{
